d102412825e8a95d016922372c3300a6fe80f544
Ashish Binu <ashishbinu90@gmail.com>
2023-07-07 10:47:41 +0530

    :goal_net: Add error handling using anyhow crate

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,23 @@
+use anyhow::Result;
 use clap::{ArgGroup, Parser};
-
 use scraper::{Html, Selector};
 
 #[derive(Parser)]
 #[clap(version, about)]
-#[clap(group(
-            ArgGroup::new("wd")
-                .required(true)
-                .args(&["word"]),
-        ))]
+#[clap(group(ArgGroup::new("wd").required(true).args(&["word"])))]
 struct Args {
     /// The word to find meaning of
     word: String,
 }
 
-fn main() {
+fn main() -> Result<()> {
     let args = Args::parse();
-    let meaning = get_meaning(&args.word).unwrap();
+    let meaning = get_meaning(&args.word)?;
     println!("{}", meaning);
+    Ok(())
 }
 
-fn get_meaning(query: &str) -> Result<String, ureq::Error> {
+fn get_meaning(query: &str) -> Result<String> {
     let html: String =
         ureq::get(format!("https://www.urbandictionary.com/define.php?term={}", query).as_ref())
             .call()?
