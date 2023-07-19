656ab74b0ad35b945f0d1c1692af577d37dc04d7
Rubens Brandao <git@rubens.io>
2023-07-19 10:46:33 -0300

    Use the JSON API

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,49 @@
 use anyhow::{anyhow, Result};
-use clap::{ArgGroup, Parser};
-use scraper::{Html, Selector};
+use clap::{arg, Parser};
+use serde::Deserialize;
+use std::collections::HashMap;
 
 #[derive(Parser)]
-#[clap(version, about)]
-#[clap(group(ArgGroup::new("wd").required(true).args(&["word"])))]
-struct Args {
+struct ArgWord {
     /// The word to find meaning of
+    #[arg(short, long)]
     word: String,
 }
 
 fn main() -> Result<()> {
-    let args = Args::parse();
+    let args = ArgWord::parse();
     let meaning = get_meaning(&args.word)?;
     println!("{}", meaning);
     Ok(())
 }
 
+#[derive(Deserialize)]
+struct UDDefinition {
+    definition: String,
+    #[serde(flatten)]
+    _other: HashMap<String, serde_json::Value>,
+}
+
+#[derive(Deserialize)]
+struct UDDefinitions {
+    list: Vec<UDDefinition>,
+}
+
 fn get_meaning(query: &str) -> Result<String> {
-    let url = format!("https://www.urbandictionary.com/define.php?term={}", query);
-    let html: String = ureq::get(&url)
+    let url = format!("https://api.urbandictionary.com/v0/define?term={}", query);
+    let json: String = ureq::get(&url)
         .call()
         .map_err(|e| anyhow!("Failed to fetch the URL: {:?}", e))?
         .into_string()
         .map_err(|e| anyhow!("Failed to parse the response: {:?}", e))?;
 
-    let parsed_html = Html::parse_document(&html);
-    let selector = Selector::parse("meta[name='Description']")
-        .map_err(|e| anyhow!("Error during parsing using the given selector: {:?}", e))?;
-    let meta_tag = parsed_html
-        .select(&selector)
+    let parsed_json: UDDefinitions =
+        serde_json::from_str(&json).map_err(|e| anyhow!("Failed to parse the json: {:?}", e))?;
+    // return the first definition found
+    parsed_json
+        .list
+        .into_iter()
         .next()
-        .ok_or_else(|| anyhow!("No meta tag found"))?;
-
-    let meaning = meta_tag
-        .value()
-        .attr("content")
-        .ok_or_else(|| anyhow!("No content attribute found"))?
-        .to_string();
-    Ok(meaning)
+        .map(|d| d.definition.clone())
+        .ok_or_else(|| anyhow!("Definition not found"))
 }
